6fe6474d367bbb32b292acf9a35fe0e8d0b2ad71
hgshkt <nichtojestvozxc@gmail.com>
2023-11-13 18:39:43 +0200

    data loading replaced to MainView

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,6 @@
 import app.Lab1App
-import domain.data.model.LoadedData
-import domain.useCases.loadData
-import presentation.MainView
 import tornadofx.launch
-import util.pathToResources
-
-lateinit var loadedData: LoadedData
 
 fun main() {
-    loadedData = loadData(
-        materialsExcelFileName = pathToResources("речовини.xlsx"),
-        pollutionsExcelFileName = pathToResources("викиди.xlsx"),
-        enterprisesExcelFileName = pathToResources("підприємства.xlsx")
-    )
-    launch<Lab1App>(arrayOf(MainView::data to loadedData))
+    launch<Lab1App>()
 }
--- src/main/kotlin/presentation/MainView.kt
+++ src/main/kotlin/presentation/MainView.kt
@@ -1,37 +1,53 @@
 package presentation
 
 import domain.data.model.LoadedData
+import domain.useCases.loadData
+import javafx.scene.Parent
 import presentation.events.ChangeViewEvent
 import presentation.tables.EnterprisesAppTableView
 import presentation.tables.MaterialsAppTableView
 import presentation.tables.PollutionsAppTableView
 import presentation.tables.AppTableView
 import tornadofx.*
+import util.pathToResources
 
-class MainView: View() {
-    val data: LoadedData by params
-//    private val enterprisesTableView = EnterprisesAppTableView(data.enterprises)
-//    private val materialsTableView = MaterialsAppTableView(data.materials)
-//    private val pollutionsTableView = PollutionsAppTableView(data.pollutions)
-//
-//    private var table: AppTableView = EnterprisesAppTableView(data.enterprises)
+class MainView : View() {
+    private var data: LoadedData = loadData(
+        materialsExcelFileName = pathToResources("речовини.xlsx"),
+        pollutionsExcelFileName = pathToResources("викиди.xlsx"),
+        enterprisesExcelFileName = pathToResources("підприємства.xlsx")
+    )
+    private val enterprisesTableView = EnterprisesAppTableView(data.enterprises)
+    private val materialsTableView = MaterialsAppTableView(data.materials)
+    private val pollutionsTableView = PollutionsAppTableView(data.pollutions)
 
-    override val root = vbox {
-//        button("enterprises") {
-//            action {
-//                table = enterprisesTableView
-//            }
-//        }
-//        button("materials") {
-//            action {
-//                table = materialsTableView
-//            }
-//        }
-//        button("pollutions") {
-//            action {
-//                table = pollutionsTableView
-//            }
-//        }
-//        table
+    private var table: AppTableView = EnterprisesAppTableView(data.enterprises)
+
+    override val root: Parent
+
+    init {
+        root = vbox {
+            button("enterprises") {
+                action {
+                    table = enterprisesTableView
+                }
+            }
+            button("materials") {
+                action {
+                    table = materialsTableView
+                }
+            }
+            button("pollutions") {
+                action {
+                    table = pollutionsTableView
+                }
+            }
+            table.root
+            MyLabel().root
+        }
     }
+}
+
+class MyLabel: View() {
+    override val root = label("qwer")
 }
