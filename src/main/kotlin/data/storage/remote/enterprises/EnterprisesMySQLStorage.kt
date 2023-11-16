48da317fa45f9c280bbb581fdc7bc259cdf52eb3
hgshkt <nichtojestvozxc@gmail.com>
2023-11-16 07:13:31 +0200

    getById fun in material and enterprises storages

--- src/main/kotlin/data/storage/remote/enterprises/EnterprisesMySQLStorage.kt
+++ src/main/kotlin/data/storage/remote/enterprises/EnterprisesMySQLStorage.kt
@@ -2,16 +2,15 @@ package data.storage.remote.enterprises
 
 import data.storage.DatabaseConnectionData
 import data.storage.remote.enterprises.model.RemoteEnterprise
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.ResultSet
-import java.sql.SQLException
+import data.storage.remote.materials.model.RemoteMaterial
+import java.sql.*
 
 class EnterprisesMySQLStorage(
     private val connectionData: DatabaseConnectionData
 ): EnterpriseRemoteStorage {
 
     private val getAllQuery = "SELECT * FROM enterprises"
+    private val getByIdQuery = "SELECT * FROM enterprises WHERE id = ?"
 
     private val columnIdName = "id"
     private val columnNameName = "enterprise_name"
@@ -33,20 +32,9 @@ class EnterprisesMySQLStorage(
             val resultSet: ResultSet = statement.executeQuery(getAllQuery)
 
             while (resultSet.next()) {
-                val id = resultSet.getInt(columnIdName)
-                val name = resultSet.getString(columnNameName)
-                val activity = resultSet.getString(columnActivityName)
-                val belonging = resultSet.getString(columnBelongingName)
-                val location = resultSet.getString(columnLocationName)
-
-                val enterprise = RemoteEnterprise(
-                    id = id,
-                    name = name,
-                    activity = activity,
-                    belonging = belonging,
-                    location = location
+                enterprises.add(
+                    element = getEnterpriseFromResultSet(resultSet)
                 )
-                enterprises.add(enterprise)
             }
 
             resultSet.close()
@@ -57,5 +45,56 @@ class EnterprisesMySQLStorage(
             e.printStackTrace()
         }
         return enterprises
+    }
+
+    override fun getById(id: Int): RemoteEnterprise? {
+        try {
+            val connection: Connection = DriverManager.getConnection(
+                connectionData.url,
+                connectionData.user,
+                connectionData.password
+            )
+
+            val preparedStatement: PreparedStatement =
+                connection.prepareStatement(getByIdQuery)
+
+            preparedStatement.setInt(1, id)
+
+            val resultSet: ResultSet = preparedStatement.executeQuery(getByIdQuery)
+
+            var enterprise: RemoteEnterprise? = null
+
+            if (resultSet.next()) {
+                enterprise = getEnterpriseFromResultSet(resultSet)
+            }
+
+            resultSet.close()
+            preparedStatement.close()
+            connection.close()
+
+            return enterprise
+
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    private fun getEnterpriseFromResultSet(
+        resultSet: ResultSet
+    ): RemoteEnterprise {
+        val id = resultSet.getInt(columnIdName)
+        val name = resultSet.getString(columnNameName)
+        val activity = resultSet.getString(columnActivityName)
+        val belonging = resultSet.getString(columnBelongingName)
+        val location = resultSet.getString(columnLocationName)
+
+        return RemoteEnterprise(
+            id = id,
+            name = name,
+            activity = activity,
+            belonging = belonging,
+            location = location
+        )
     }
 }
--- src/main/kotlin/data/storage/remote/materials/MaterialMySQLStorage.kt
+++ src/main/kotlin/data/storage/remote/materials/MaterialMySQLStorage.kt
@@ -1,17 +1,17 @@
 package data.storage.remote.materials
 
 import data.storage.DatabaseConnectionData
+import data.storage.remote.enterprises.model.RemoteEnterprise
 import data.storage.remote.materials.model.RemoteMaterial
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.ResultSet
-import java.sql.SQLException
+import domain.model.Material
+import java.sql.*
 
 class MaterialMySQLStorage(
     private val connectionData: DatabaseConnectionData
 ) : MaterialRemoteStorage {
 
     private val getAllQuery = "SELECT * FROM materials"
+    private val getByIdQuery = "SELECT * FROM materials WHERE id = ?"
 
     private val columnIdName = "id"
     private val columnNameName = "material_name"
@@ -33,18 +33,9 @@ class MaterialMySQLStorage(
             val resultSet: ResultSet = statement.executeQuery(getAllQuery)
 
             while (resultSet.next()) {
-                val id = resultSet.getInt(columnIdName)
-                val name = resultSet.getString(columnNameName)
-                val gdk = resultSet.getDouble(columnGdkName)
-                val dangerClass = resultSet.getInt(columnDangerClassName)
-
-                val material = RemoteMaterial(
-                    id = id,
-                    name = name,
-                    gdk = gdk,
-                    dangerClass = dangerClass
+                materials.add(
+                    element = getMaterialFromResultSet(resultSet)
                 )
-                materials.add(material)
             }
             resultSet.close()
             statement.close()
@@ -53,5 +44,54 @@ class MaterialMySQLStorage(
             e.printStackTrace()
         }
         return materials
+    }
+
+    override fun getById(id: Int): RemoteMaterial? {
+        try {
+            val connection: Connection = DriverManager.getConnection(
+                connectionData.url,
+                connectionData.user,
+                connectionData.password
+            )
+
+            val preparedStatement: PreparedStatement =
+                connection.prepareStatement(getByIdQuery)
+
+            preparedStatement.setInt(1, id)
+
+            val resultSet: ResultSet = preparedStatement.executeQuery(getByIdQuery)
+
+            var material: RemoteMaterial? = null
+
+            if (resultSet.next()) {
+                material = getMaterialFromResultSet(resultSet)
+            }
+
+            resultSet.close()
+            preparedStatement.close()
+            connection.close()
+
+            return material
+
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    private fun getMaterialFromResultSet(
+        resultSet: ResultSet
+    ): RemoteMaterial {
+        val id = resultSet.getInt(columnIdName)
+        val name = resultSet.getString(columnNameName)
+        val gdk = resultSet.getDouble(columnGdkName)
+        val dangerClass = resultSet.getInt(columnDangerClassName)
+
+        return RemoteMaterial(
+            id = id,
+            name = name,
+            gdk = gdk,
+            dangerClass = dangerClass
+        )
     }
 }
