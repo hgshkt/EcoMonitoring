8e2c694333e7b09efe6b4ac62fadd58fd79e7491
hgshkt <nichtojestvozxc@gmail.com>
2023-12-10 02:59:03 +0200

    LoadYearConcentrationsFromExcelUseCase changed to risk calculating

--- src/main/kotlin/domain/useCases/loadFromExcel/LoadYearConcentrationsFromExcelUseCase.kt
+++ src/main/kotlin/domain/useCases/loadFromExcel/LoadYearConcentrationsFromExcelUseCase.kt
@@ -1,18 +1,32 @@
 package domain.useCases.loadFromExcel
 
-import domain.data.mappers.toRemote
+import domain.data.obtained.calculators.RiskCalculator
+import domain.data.repository.material.remote.MaterialsRemoteRepository
 import domain.data.repository.yearConcentration.excel.ExcelYearConcentrationRepository
 import domain.data.repository.yearConcentration.remote.YearConcentrationsRemoteRepository
+import domain.model.data.remote.RemoteYearConcentrationData
 
 class LoadYearConcentrationsFromExcelUseCase(
     private val remoteRepository: YearConcentrationsRemoteRepository,
-    private val excelRepository: ExcelYearConcentrationRepository
+    private val excelRepository: ExcelYearConcentrationRepository,
+    private val materialsRemoteRepository: MaterialsRemoteRepository
+    private val riskCalculator: RiskCalculator
 ) {
     fun execute(
         filePath: String
     ) {
-        val excelData = excelRepository.getData(filePath)
-        val remoteData = excelData.toRemote()
+        val concentrations = excelRepository.getData(filePath).concentrations
+        val materials = materialsRemoteRepository.getData().materials
+
+        val calculatedConcentrations = riskCalculator.calculateRisk(
+            materials,
+            concentrations
+        )
+
+        val remoteData = RemoteYearConcentrationData(
+            concentrations = calculatedConcentrations
+        )
+
         remoteRepository.addData(remoteData)
     }
 }
