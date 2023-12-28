779a7e226560f81820fe37986f5144c4f55cead2
hgshkt <nichtojestvozxc@gmail.com>
2023-12-28 22:17:54 +0200

    risk calculating after creating pollution

--- src/main/kotlin/domain/useCases/create/CreatePollutionUseCase.kt
+++ src/main/kotlin/domain/useCases/create/CreatePollutionUseCase.kt
@@ -3,21 +3,26 @@ package domain.useCases.create
 import data.repository.pollutions.remote.PollutionMySQLRepository
 import data.storage.DatabaseConnectionData
 import data.storage.remote.pollutions.PollutionsMySQLStorage
+import domain.data.obtained.calculators.RiskCalculator
+import domain.data.repository.material.remote.MaterialsRemoteRepository
 import domain.data.repository.pollution.remote.PollutionsRemoteRepository
 import domain.model.Pollution
 import java.sql.SQLException
 import java.sql.SQLIntegrityConstraintViolationException
 
 class CreatePollutionUseCase(
-    private val repository: PollutionsRemoteRepository = PollutionMySQLRepository(
-        storage = PollutionsMySQLStorage(
-            connectionData = DatabaseConnectionData()
-        )
-    )
+    private val repository: PollutionsRemoteRepository,
+    private val riskCalculator: RiskCalculator,
+    private val materialsRemoteRepository: MaterialsRemoteRepository
 ) {
     fun execute(pollution: Pollution, sqlException: () -> Unit) {
         try {
+
+            val material = materialsRemoteRepository.getByName(pollution.materialName)
+            riskCalculator.calculateRisk(material, pollution)
+
             repository.add(pollution)
+
         } catch (exception: SQLIntegrityConstraintViolationException) {
             sqlException()
         }
--- src/main/kotlin/presentation/screens/creating/createPollution/CreatePollutionScreenUseCases.kt
+++ src/main/kotlin/presentation/screens/creating/createPollution/CreatePollutionScreenUseCases.kt
@@ -1,10 +1,13 @@
 package presentation.screens.creating.createPollution
 
+import data.obtained.RiskCalculatorImpl
 import data.repository.enterprises.remote.EnterpriseMySQLRepository
 import data.repository.materials.remote.MaterialMySQLRepository
+import data.repository.pollutions.remote.PollutionMySQLRepository
 import data.storage.DatabaseConnectionData
 import data.storage.remote.enterprises.EnterprisesMySQLStorage
 import data.storage.remote.materials.MaterialMySQLStorage
+import data.storage.remote.pollutions.PollutionsMySQLStorage
 import domain.useCases.create.CreatePollutionUseCase
 import domain.useCases.get.GetEnterprisesFromRemoteRepositoryUseCase
 import domain.useCases.get.GetMaterialsFromRemoteRepositoryUseCase
@@ -26,5 +29,17 @@ data class CreatePollutionScreenUseCases(
             )
         )
     ),
-    val createUseCase: CreatePollutionUseCase = CreatePollutionUseCase()
+    val createUseCase: CreatePollutionUseCase = CreatePollutionUseCase(
+        repository = PollutionMySQLRepository(
+            storage = PollutionsMySQLStorage(
+                connectionData = DatabaseConnectionData()
+            )
+        ),
+        riskCalculator = RiskCalculatorImpl(),
+        materialsRemoteRepository = MaterialMySQLRepository(
+            storage = MaterialMySQLStorage(
+                connectionData = DatabaseConnectionData()
+            )
+        )
+    )
 )
