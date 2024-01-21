fecb7f0e30595af83e96d8a49603f96affb2cf66
hgshkt <nichtojestvozxc@gmail.com>
2024-01-21 05:46:23 +0200

    CreateTaxRateUseCase catch sql exception

--- src/main/kotlin/domain/useCases/create/CreateTaxRateUseCase.kt
+++ src/main/kotlin/domain/useCases/create/CreateTaxRateUseCase.kt
@@ -2,11 +2,16 @@ package domain.useCases.create
 
 import domain.data.repository.taxRate.remote.TaxRateRemoteRepository
 import domain.model.TaxRate
+import java.sql.SQLIntegrityConstraintViolationException
 
 class CreateTaxRateUseCase(
     private val repository: TaxRateRemoteRepository
 ) {
-    fun execute(taxRate: TaxRate) {
-        repository.add(taxRate)
+    fun execute(taxRate: TaxRate, sqlException: () -> Unit) {
+        try {
+            repository.add(taxRate)
+        } catch (exception: SQLIntegrityConstraintViolationException) {
+            sqlException()
+        }
     }
 }
